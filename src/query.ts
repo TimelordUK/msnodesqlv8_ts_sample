fa677ac94173138ac1b823ff91ab97e82a63acd2
TimelordUK <sjames8888@gmail.com>
2025-06-29 18:43:14 +0100

    add a git ignore

--- src/query.ts
+++ src/query.ts
@@ -2,8 +2,8 @@ import sql from 'msnodesqlv8'
 import Connection = MsNodeSqlV8.Connection
 import ConnectionPromises = MsNodeSqlV8.ConnectionPromises
 async function t() {
-    const connectString = 'Driver={ODBC Driver 18 for SQL Server}; Server=DESKTOP-VIUCH90;UID=linux; PWD=linux; Database=node;Encrypt=no'
-    const con:Connection = await sql.promises.open(connectString)
+    const connectionString = "Driver={ODBC Driver 18 for SQL Server};Server=127.0.0.1,1433;Database=node;UID=node_user;PWD=StrongPassword123!;TrustServerCertificate=yes;Connect Timeout=10";
+    const con:Connection = await sql.promises.open(connectionString)
     const promises:ConnectionPromises  = con.promises
     const res = await promises.query('select @@servername as server')
     console.log(JSON.stringify(res, null, 4))
